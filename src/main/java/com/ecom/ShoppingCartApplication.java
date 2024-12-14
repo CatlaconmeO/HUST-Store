ff5bcffb779f9d3ee1056ccdabbb16e7954c569f
dwclam <Lam.ND235360@sis.hust.edu.vn>
2024-12-14 21:42:00 +0700

    update

--- src/main/java/com/ecom/ShoppingCartApplication.java
+++ src/main/java/com/ecom/ShoppingCartApplication.java
@@ -2,7 +2,11 @@ package com.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*Phạm Đức Dũng
+  Trần Viết Tùng Lâm
+  Trịnh PHú Thái
+  Nguyễn Đức Lâm
+*/
 @SpringBootApplication
 public class ShoppingCartApplication {
 
